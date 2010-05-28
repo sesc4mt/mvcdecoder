7a166cebe836fd944ac2695407a6ca15defd03f8
edwardtoday <edwardtoday@34199c9c-95aa-5eba-f35a-9ba8a8a04cd7>
2010-05-28 06:56:33 +0000

    LaTeX_manual & lshort-cn-3.20

---
+++ 3D Project/thesis/lshort-cn-3.20/make-ps.bat
@@ -0,0 +1,20 @@
+@echo off
+latex -src lshort.tex
+makeindex -o lshort.ind lshort.idx
+latex -src lshort.tex
+latex -src lshort.tex
+dvips lshort.dvi
+#ps2pdf lshort.ps
+dvipdfmx lshort.dvi
+del *.aux
+del *.toc
+del *.lof
+del *.lot
+del *.ilg
+del *.idx
+del *.ind
+del *.out
+del *.log
+del *.exa
+@echo on
+pause
