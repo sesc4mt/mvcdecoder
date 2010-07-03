411c3f8d55ddaa4f8ca93edb737eccec8bf64b1b
edwardtoday <edwardtoday@34199c9c-95aa-5eba-f35a-9ba8a8a04cd7>
2010-07-03 21:16:59 +0000

    submit version 20100703

---
+++ 3D Project/提交版本/src/data/src/batchEncode.bat
@@ -0,0 +1,9 @@
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 0
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 2
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 1
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 4
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 3
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 6
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 5
+H264AVCEncoderLibTestStatic.exe -vf MVC.cfg 7
+MVCBitStreamAssemblerStatic.exe -vf assembler.cfg
